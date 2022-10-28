7b56eb00223e0f49913a63ed12bc78c4377670b8
PabloRealsola <95261802+PabloRealsola@users.noreply.github.com>
2022-10-27 23:54:12 -0500

    Add files via upload

---
+++ js/imagenes.js
@@ -0,0 +1,27 @@
+
+let Animales = [];
+let reverso = "img/reverso.jpg"
+
+function cargarImagenes() {
+
+	
+
+	Animales[1] = "img/Animales/abeja.jpg";
+	Animales[0] = "img/Animales/cabra.jpg";
+	Animales[2] = "img/Animales/cedo.jpg";
+	Animales[3] = "img/Animales/gato.jpg";
+	Animales[4] = "img/Animales/hipopotamo.jpg";
+	Animales[5] = "img/Animales/leon.jpg";
+	Animales[6] = "img/Animales/lobo.jpg";
+	Animales[7] = "img/Animales/mono.jpg";
+	Animales[8] = "img/Animales/pantera.jpg";
+	Animales[9] = "img/Animales/perro.jpg";
+	Animales[10] = "img/Animales/pez.jpg";
+	Animales[11] = "img/Animales/tigre.jpg";
+	Animales[12] = "img/Animales/toro.jpg";
+	Animales[13] = "img/Animales/tortuga.jpg";
+	Animales[14] = "img/Animales/vaca.jpg";
+	Animales[15] = "img/Animales/zerba.jpg";
+
+
+}
